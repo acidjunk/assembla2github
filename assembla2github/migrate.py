219aab6490a1a42718ac04017e492ac5fb562bf4
acidjunk <rene.dohmen1@kpn.com>
2017-05-16 01:00:22 +0200

    A script to copy all assembla git repo's to github

--- assembla2github/migrate.py
+++ assembla2github/migrate.py
@@ -1,7 +1,9 @@
-from conf import *
-
+import os
 from github import Github
 from assembla import API
+
+from conf import *
+
 
 # Assembla instance
 assembla = API(key=ASSEMBLA_KEY, secret=ASSEMBLA_SECRET)
@@ -11,18 +13,24 @@ github = Github(GITHUB_USER, GITHUB_PASSWORD)
 
 def clone_git(url):
     print('Cloning url: {url}'.format(url=url))
+    os.system('git clone --bare {url}'.format(url=url))
 
 def get_repo_name(url):
     # Example: git@git.assembla.com:spacename.gitname.git
     return url.split(':')[1][0:-4]
+
+def create_repo(name):
+    github.get_user().create_repo(name, private=True)
+
+def push_repo(name):
+    os.chdir('{name}.git'.format(name=name))
+    os.system('git push --mirror git@github.com:{user}/{name}.git'.format(user=GITHUB_USER, name=name))
+    os.chdir('..')
 
 for space in assembla.spaces():
     print("Working for {space}".format(space=space['name']))
     for git_repo in space.tools(type='GitTool'):
         repo_name = get_repo_name(git_repo['url'])
         clone_git(git_repo['url'])
-
-
-## Then play with your Github objects:
-#for repo in github.get_user().get_repos():
-#    print repo.name
+        create_repo(repo_name)
+        push_repo(repo_name)
